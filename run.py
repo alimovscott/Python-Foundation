2cd4b691b514ddd51cc33af490719e7520140a62
alimovscott <abduqodiralimov23@icloud.com>
2026-04-07 22:50:48 +0900

    feat: python system variable vs special method via dunder

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("hello world")
+# Dunder __builtins__, __init__,
+message = "hello world"
+print(message)
+print("PYTHON: everything is object") 
+
+''' In python, there are builtin tools:
+(1) TYPES => int float str list dict
+(2) FUNCTION  => print() len() input() type() str() int()
+(3) CONSTANTS => True False None
+'''
+
+print(dir(__builtins__))
