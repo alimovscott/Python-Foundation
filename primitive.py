906ef25871e3bfe8e1994839e99b7247e32a101c
alimovscott <abduqodiralimov23@icloud.com>
2026-04-07 23:41:26 +0900

    feat: learn numbers python

---
+++ primitive.py
@@ -0,0 +1,15 @@
+print("=========")
+# in JAVA, variable name of storege location
+# in Python, variables is named reference!
+
+count = 100
+count_type = type(count)
+print("count:", count)
+print(count_type)
+
+print(f"i am {count} years old")
+
+result1 = count.bit_count()
+result2 = count.numerator
+
+print(result1, result2)
