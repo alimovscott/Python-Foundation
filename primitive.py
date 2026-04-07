430d4e75df0ddb5478034cee19d6f3edf002ddee
alimovscott <abduqodiralimov23@icloud.com>
2026-04-08 00:01:36 +0900

    feat: learn python boolean

--- primitive.py
+++ primitive.py
@@ -29,3 +29,21 @@ print(f"asd{result}")
 result = course.replace("AI python fullstack", "Scott")
 
 print(result)
+
+
+print("====boolean====")
+# functions > type() input() bool() int() str()
+
+y = input("give your value for y ")
+print(y)
+
+result = y.isnumeric()
+print(result)
+
+
+# TRUTHY vs FALSY value
+# TRUTH > true 100 , -100, "SCOTT"
+# FALSY > false 0, "" , nan
+
+test_falsy = "" 
+print("THE Falsy", bool(test_falsy))
