e13340f8690ef8706c06fbcbedaf451ad42b6bd2
alimovscott <abduqodiralimov23@icloud.com>
2026-04-09 18:04:54 +0900

    feat: learn python objects

---
+++ objects.py
@@ -0,0 +1,32 @@
+'''
+Docstring for objects
+(1) What is object
+(2) Iterable objects & RANGE
+(3) DICTONARY
+(4) Error handling
+'''
+
+import array # package/module
+import math # package/module
+from math import ceil
+
+
+print("==== What is object ====")
+
+#An object has state and method properties
+#Everything is object in python
+
+print(type("hello world"))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+
+#PARADIGMA > Functional Programming & OOP
+# OOP 4 CONSEPTS > ABCTRACTION | ENCAPSuLATION | INHERTENCE | POLIFORMISM
+
+result1 = ceil(97.6) #CALL
+print(result1)
+
+
