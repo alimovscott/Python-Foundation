5daef086d4f627cd66269c7815876a111d865ddb
alimovscott <abduqodiralimov23@icloud.com>
2026-04-09 18:38:43 +0900

    Learn Error handling

--- objects.py
+++ objects.py
@@ -29,4 +29,33 @@ print(type(math))
 result1 = ceil(97.6) #CALL
 print(result1)
 
+print("==== Error handling System ====")
+
+car_dist = dict(name = "Tayota", year = "2025", electric = True )
+
+try:
+    print("passed here")
+    result2 = car_dist["origin"]
+except KeyError as err:
+    print("No origin date", err)
+else:
+    print("Execetud successfully without errors")
+finally:
+    print("Final closing logic")
+
+
+try:
+    print("passed here2")
+    result2 = car_dist["year"]
+except KeyError as err:
+    print("No origin date", err)
+else:
+    print("Execetud successfully without errors")
+finally:
+    print("Final closing logic")
+
+
+
+
+
 
